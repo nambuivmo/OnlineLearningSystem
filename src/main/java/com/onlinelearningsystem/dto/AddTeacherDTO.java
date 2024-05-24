cdf5345b4f6f4953176a6a79d2be69c768d8e222
nambuivmo <nambv2@vmogroup.com>
2024-05-24 15:42:18 +0700

    Add teacher(additional)

---
+++ src/main/java/com/onlinelearningsystem/dto/AddTeacherDTO.java
@@ -0,0 +1,19 @@
+package com.onlinelearningsystem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddTeacherDTO {
+    private Long idAccount;
+    private String firstName;
+    private String lastName;
+    private LocalDate dob;
+    private boolean gender;
+    private String address;
+    private String phoneNumber;
+}
